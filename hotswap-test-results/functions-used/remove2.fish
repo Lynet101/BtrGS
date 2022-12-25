fe21a17c657337d3669b43fb25f019f216665b6f
Sebastian Lindau-Skands <47390147+Lynet101@users.noreply.github.com>
2022-12-25 11:20:07 +0100

    Delete hotswap-test-results directory

--- hotswap-test-results/functions-used/remove2.fish
+++
@@ -1,3 +0,0 @@
-function remove2 --wraps='echo 1 > /sys/bus/pci/devices/0000:01:00.1/remove' --description 'alias remove2 echo 1 > /sys/bus/pci/devices/0000:01:00.1/remove'
-  echo 1 > /sys/bus/pci/devices/0000:01:00.1/remove $argv; 
-end
--- hotswap-test-results/functions-used/unbindall.fish
+++
@@ -1,3 +0,0 @@
-function unbindall --wraps='unbind1 && unbind2' --description 'alias unbindall unbind1 && unbind2'
-  unbind1 && unbind2 $argv; 
-end
