6644194164414b0d1cbb20e7b993422cb3ed8e36
Sebastian Lindau-Skands <47390147+Lynet101@users.noreply.github.com>
2022-12-29 09:21:20 +0100

    OpenGL multigpu render test sketch

---
+++ indev/OpenGL testing/Basic tests with multi-gpu-rendering/GL-test-01.py
@@ -0,0 +1,18 @@
+#imports
+
+"""
+Function to load gif, and split up to individual frames
+frames should be sent to 'gpu1' by default, but on demand, frames should switch to 'gpu2'
+"""
+
+"""
+Rendering engine for gpu1
+"""
+
+"""
+Rendering engine for gpu2
+"""
+
+"""
+Potential loopback, to allow for synchronisation between gpu's
+"""
